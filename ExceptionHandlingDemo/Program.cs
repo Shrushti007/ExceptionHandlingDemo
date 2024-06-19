949e8490563d370754b62e19dcb46e44fd5f2fde
Ankit Jagtap <ankit.jagtap50@gmail.com>
2024-06-19 20:05:21 +0530

    Add project files.

---
+++ ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExceptionProjectLibrary;
+namespace ExceptionHandlingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			Customer c = new Customer();
+			try
+			{
+                Console.WriteLine("Enter the cust id:");
+                c.Custid=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter cust name:");
+                c.CustName=Console.ReadLine();
+                Console.WriteLine("Enter cust city");
+                c.City=Console.ReadLine();
+            }
+			catch (ArgumentNullException ex)
+			{
+
+                Console.WriteLine( ex.Source);
+                
+            }
+            catch (AccessViolationException es)
+            {
+                Console.WriteLine(es.Message);
+            }
+            catch(CustomerNotFoundException et)
+            {
+                Console.WriteLine(et.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block called..");
+            }
+        }
+    }
+}
---
+++ ExceptionProjectLibrary/Customer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace ExceptionProjectLibrary
+
+{
+    public class Customer
+    {
+        int _custid;
+        string _custName;
+        string _city;
+        public int Custid 
+        {
+            get
+            { 
+                return _custid;
+            } 
+            set
+            {
+                if (value > 0 && value<2000) 
+                {
+                    _custid = value;
+                }
+                else if (value > 2000)
+                {
+                    throw new CustomerNotFoundException("This is not valid number");
+                }
+                else
+                {
+                    throw new ArgumentNullException("Customer Id must be positive Number");
+                }
+                
+            }
+        }
+        public string CustName
+        { 
+            get
+            { 
+                return _custName; 
+            } 
+            set 
+            {
+                if(string.IsNullOrEmpty(value) )
+                   { 
+                    throw new ArgumentNullException("Customer Name Cannot be Null Or Empty");
+                   }
+                else if(value.StartsWith(" "))
+                {
+                    throw new AccessViolationException("Name Cannot start with a space");
+                }
+                else
+                {
+                    _custName = value;
+
+                }
+            }
+        }
+        public string City 
+        {
+            get
+            {
+                return _city; 
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Customer city Cannot be Null Or Empty");
+                }
+                else
+                {
+                    _city = value;
+                }
+            }
+        }
+    }
+}
